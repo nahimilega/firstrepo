f0fe932b0754c934504b3957c616886a40693c04
nahimilega <archit18221@iiitd.ac.in>
2018-10-01 18:28:58 +0530

    experiment 1

---
+++ robot.py
@@ -0,0 +1,30 @@
+xstart=int(input('Enter the starting x coordinate   '))
+ystart=int(input('Enter the starting y coordinate   '))
+x=xstart
+y=ystart
+flag=0
+while(flag==0):
+	order=input()
+	if(order=='STOP'):
+		flag=5
+	else :
+		step=int(order[-1])
+
+		direction=order[:-2]
+		if direction=='RIGHT'	:
+			y=y+step
+		else if direction=='LEFT' :
+			y=y-step
+		else if direction=='UP' :
+			x=x+step
+		else if direction=='DOWN' :
+			x=x-step
+		else :
+			print("Please Enter Correct Input ")
+
+
+print("The final x coordinate is ",x)
+print("The final y coordinate is ",y)
+dist=((x-xstart)**2+(y-ystart)**2)**0.5
+print("The total distance traveled is ",dist)
+
